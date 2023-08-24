9e7b5d5e434c89ab5022d3d4261e9135cb7697cc
pankaj-mahor <mahorpankaj12@gmail.com>
2023-08-24 22:19:50 +0530

    section define for website

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-	return <div>Hellp</div>;
+	return (
+		<main className="relative">
+			nav
+			<section className="xl:padding-1 wide:padding-r padding-b">hero</section>
+			<section className="padding   ">popular product</section>
+			<section className="padding   ">super quality</section>
+			<section className="padding-x py-10  ">services</section>
+			<section className="padding   ">special offer</section>
+			<section className="padding  bg-pale-blue ">C review</section>
+			<section className="padding-x sm:py-32 py-16 w-full  ">subscribe</section>
+			<section className="padding  bg-black padding-x padding-t pb-8 ">
+				footers
+			</section>
+		</main>
+	);
 }
 
 export default App;
