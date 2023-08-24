db5a704e727d79d67b1239ed97efc227fc0df24a
pankaj-mahor <mahorpankaj12@gmail.com>
2023-08-24 22:23:41 +0530

    imported componet

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,41 @@ import { useState } from "react";
 
 import "./App.css";
 import CustomerReview from "./sections/CustomerReview";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
 
 function App() {
 	return (
 		<main className="relative">
 			nav
-			<section className="xl:padding-1 wide:padding-r padding-b">hero</section>
-			<section className="padding   ">popular product</section>
-			<section className="padding   ">super quality</section>
-			<section className="padding-x py-10  ">services</section>
-			<section className="padding   ">special offer</section>
+			<section className="xl:padding-1 wide:padding-r padding-b">
+				<Hero />
+			</section>
+			<section className="padding   ">
+				<PopularProducts />
+			</section>
+			<section className="padding   ">
+				<SuperQuality />
+			</section>
+			<section className="padding-x py-10  ">
+				<Services />
+			</section>
+			<section className="padding   ">
+				<SpecialOffer />
+			</section>
 			<section className="padding  bg-pale-blue ">
 				<CustomerReview />
 			</section>
-			<section className="padding-x sm:py-32 py-16 w-full  ">subscribe</section>
+			<section className="padding-x sm:py-32 py-16 w-full  ">
+				<Subscribe />
+			</section>
 			<section className="padding  bg-black padding-x padding-t pb-8 ">
-				footers
+				<Footer />
 			</section>
 		</main>
 	);
