b911e7cadaa02557109ab57ac89e43a5b19cb46b
pankaj-mahor <mahorpankaj12@gmail.com>
2023-08-25 22:02:53 +0530

    partial hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Hero = () => {
-	return <div>Hero</div>;
+	return (
+		<section
+			id="hero"
+			className="w-full flex xl:flex-row flex-col justify-center
+  
+  min-h-screen gap-10 max-container"
+		>
+			<div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-lg:padding-x pt-28">
+				<p className="">Our summer collection</p>
+
+				<h1>
+					<span>The New Arrival</span>
+					<br />
+					<span>Nike </span>
+					Shoes
+				</h1>
+
+				<p>
+					Discover styles nike arrivals, quality comfort, and innovation for
+					your active life.
+				</p>
+			</div>
+		</section>
+	);
 };
 
 export default Hero;
