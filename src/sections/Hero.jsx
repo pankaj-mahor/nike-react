d1922990799bc6006962d52d74904f2c064b98c0
pankaj-mahor <mahorpankaj12@gmail.com>
2023-08-29 22:09:16 +0530

    fix: hero breaking in mobile

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
 					Our summer collection
 				</p>
 
-				<h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+				<h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
 					<span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
 						The New Arrival
 					</span>
