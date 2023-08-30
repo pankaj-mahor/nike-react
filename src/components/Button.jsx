7654d39b16a62ead2117043f9993473b191e5244
pankaj-mahor <mahorpankaj12@gmail.com>
2023-08-30 11:31:34 +0530

    Button Support for full width

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,10 +6,13 @@ const Button = ({
 	backgroundColor,
 	textColor,
 	borderColor,
+	fullWidth,
 }) => {
 	return (
 		<button
 			className={`flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none  rounded-full  
+			
+		${fullWidth && "w-full"}
 		${
 			backgroundColor
 				? `${backgroundColor} ${textColor} ${borderColor}`
